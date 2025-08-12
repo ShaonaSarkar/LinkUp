ff8a4416a2075f093f1c1ebe495f18b4df4d82b3
ShaonaSarkar <sarkarshaona963@gmail.com>
2025-08-12 23:36:13 +0530

    creating the home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Profilepage from './pages/Profilepage'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className = "bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/ProfilePage' element={<Profilepage/>}/>
+      </Routes>
+    </div>
   )
 }
 
